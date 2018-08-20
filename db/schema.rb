16b9b738915220de5ee913d01fbab9da2f2c9775
Raymond Lee <rayjaelee@gmail.com>
2018-08-20 12:10:34 +0800

    Added and created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_103515) do
+ActiveRecord::Schema.define(version: 2018_08_20_035217) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
